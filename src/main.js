a82f21374fa1889784a6ec452fffea37c3bc031d
viktordark <viktordark2003@gmail.com>
2021-11-14 13:16:43 +0200

    day4

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
-import App from './components/App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-    
-    Vue.use(VueAxios, axios)
+import VueRouter from  'vue-router'
 
+import Students from './components/Students.vue'
+import StudentInfo from './components/StudentInfo.vue'
+import App from './components/App.vue'
+
+const routes = [
+   {path: '/', component: Students},
+   {path: '/student-info/:id', component: StudentInfo, props: true},
+]
+
+const router = new VueRouter({
+   routes
+})
+    
+Vue.use( VueAxios, axios)
+Vue.use(VueRouter)
 
 new Vue({
    render: h => h(App),
    el: '#app',
+   router
 })
